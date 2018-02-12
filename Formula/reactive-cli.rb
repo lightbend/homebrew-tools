de9c6f9aee34b9985a64bf13f809cfcefde4baf0
Jason Longshore <longshorej@users.noreply.github.com>
2018-02-12 13:11:53 -0600

    Update reactive-cli.rb

--- Formula/reactive-cli.rb
+++ Formula/reactive-cli.rb
@@ -6,9 +6,9 @@ class ReactiveCli < Formula
   depends_on "re2"
   depends_on "openssl"
 
-  url "https://bintray.com/lightbend/generic/download_file?file_path=reactive-cli-0.8.0-Mac_OS_X-x86_64.zip"
-  version "0.8.0"
-  sha256 "ddbe409dff37370dc3b804a3164f946c3cd1b94efb4d5bcb58abf887c3e4b68f"
+  url "https://bintray.com/lightbend/generic/download_file?file_path=reactive-cli-0.9.0-Mac_OS_X-x86_64.zip"
+  version "0.9.0"
+  sha256 "d8101c5dc09379033812875161dc04e65aab1f4f3457c1ff40e7898e3c241f2a"
 
   def install
     bin.install Dir["bin/*"]
