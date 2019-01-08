36f5f27610b8415a2e205e367cc8d3f47b96cdc8
Eugene Yokota <eed3si9n@gmail.com>
2019-01-08 15:33:26 -0500

    reactive-cli 1.6.0

--- Formula/reactive-cli.rb
+++ Formula/reactive-cli.rb
@@ -6,9 +6,9 @@ class ReactiveCli < Formula
   depends_on "re2"
   depends_on "openssl"
 
-  url "https://bintray.com/lightbend/generic/download_file?file_path=reactive-cli-1.3.2-Mac_OS_X-x86_64.zip"
-  version "1.3.2"
-  sha256 "3d2d33a8ff6f05b3b191388a15dc3d9e11d6272aa626fe9e478216a3da9d6ecd"
+  url "https://bintray.com/lightbend/generic/download_file?file_path=reactive-cli-1.6.0-Mac_OS_X-x86_64.zip"
+  version "1.6.0"
+  sha256 "172066a42ddceba7396a91ff8fb30f46fc60f5f52cc7488b0e3e72fb7c2c04c6"
 
   def install
     bin.install Dir["bin/*"]
